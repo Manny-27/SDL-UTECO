9c3c890eb4747719168fbecbdac23ad3891e4fb4
Manny-27 <mannyalberto2763@gmail.com>
2024-05-13 23:32:41 -0300

    boton de buscar completamente funcionando, incluso por atajo de teclado

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -11,10 +11,12 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
-import { Popover, PopoverTrigger, PopoverContent } from "@radix-ui/react-popover";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 import { TrashBox } from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 const Navigation = () => {
+    const search = useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const create = useMutation(api.documents.create);
@@ -128,7 +130,7 @@ const Navigation = () => {
                 label="Buscar"
                 icon={Search}
                 isSearch
-                onClick={() => {}}
+                onClick={search.onOpen}
                 />
                 <Item 
                 label="Ajustes"
