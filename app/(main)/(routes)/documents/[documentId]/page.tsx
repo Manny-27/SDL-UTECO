8bd4df0cdcb0eb70c18385eb136121ff601fad65
Manny-27 <mannyalberto2763@gmail.com>
2024-05-21 17:04:06 -0300

    arreglado problema a la hora de agregar iconos

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -33,11 +33,10 @@ const DocumentIdPage = ({
 
     return ( 
         <div className="pb-40">
-            <div className="h-[35vh]">
+            <div className="h-[35vh]" />
                 <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                     <Toolbar initialData={document} />
                 </div>
-            </div>
         </div>
     );
 }
--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -4,6 +4,12 @@ import { Doc } from "@/convex/_generated/dataModel"
 import { IconPicker } from "./icon-picker";
 import { Button } from "@/components/ui/button";
 import { ImageIcon, Smile, X } from "lucide-react";
+import { useRef, ElementRef, useState, use } from 'react';
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import TextareaAutosize from "react-textarea-autosize";
+import { removeIcon } from '../convex/documents';
+import { on } from "events";
 
 interface ToolbarProps {
     initialData: Doc<"documents">;
@@ -14,17 +20,64 @@ export const Toolbar = ({
     initialData,
     preview
 }: ToolbarProps) => {
+    const inputRef = useRef<ElementRef<"textarea">>(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(initialData.title);
+
+    const update = useMutation(api.documents.update);
+    const removeIcon = useMutation(api.documents.removeIcon);
+
+    const enableInput = () => {
+        if (preview) return;
+        
+        setIsEditing(true);
+        setTimeout(() => {
+            setValue(initialData.title);
+            inputRef.current?.focus();
+        }, 0);
+    };
+
+    const disableInput = () => setIsEditing(false);
+
+    const onInput = (value: string) => {
+        setValue(value);
+        update({
+            id: initialData._id,
+            title: value || "Untitled"
+        });
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLTextAreaElement>
+    ) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            disableInput();
+        }
+    };
+
+    const onIconSelect = (icon: string) => {
+        update({
+            id: initialData._id,
+            icon,
+        });
+    };
+
+    const onRemoveIcon = () => {
+        removeIcon({ id: initialData._id });
+    };
+
     return (
         <div className="pl-[54px] group relative">
             {!!initialData.icon && !preview && (
                 <div className="flex items-center gap-x-2 group/icon pt-6">
-                    <IconPicker onChange={() => {}}>
+                    <IconPicker onChange={onIconSelect}>
                         <p className="text-6xl hover:opacity-75 transition">
                             {initialData.icon}
                         </p>
                     </IconPicker>
                     <Button
-                        onClick={() => {}}
+                        onClick={onRemoveIcon}
                         className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
                         variant="outline"
                         size="icon"
@@ -38,9 +91,9 @@ export const Toolbar = ({
                     {initialData.icon}
                 </p>
             )}
-            <div className="opacity-100 group-hover:opacity-110 flex items-center gap-x-1 py-4">
+            <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
                 {!initialData.icon && !preview && (
-                <IconPicker asChild onChange={() => {}}>
+                <IconPicker asChild onChange={onIconSelect}>
                     <Button
                         className="text-muted-foreground text-xs"
                         variant="outline"
@@ -63,6 +116,24 @@ export const Toolbar = ({
                     </Button>
                 )}
             </div>
+            {isEditing && !preview ? (
+                <TextareaAutosize
+                    ref={inputRef}
+                    onBlur={disableInput}
+                    value={value}
+                    onChange={(e) => onInput(e.target.value)}
+                    onKeyDown={onKeyDown}
+                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+                    placeholder="titulo"
+                />  
+            ) : (
+                <div
+                    onClick={enableInput}
+                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+                >
+                    {initialData.title}
+                </div>
+            )}
         </div>
     )
 }
