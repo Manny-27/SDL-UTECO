01a848332ecec94e1d297b4510d747dab71a0728
Manny-27 <mannyalberto2763@gmail.com>
2024-07-02 20:03:20 -0300

    fix2

--- convex/getUsers.ts
+++
@@ -1,7 +0,0 @@
-// import { internalMutation } from "convex/server";
-// import { v } from "convex/values";
-
-// export const getUsers = internalMutation(async ({ db }) => {
-//   const users = await db.table("users").collect();
-//   return users;
-// });
